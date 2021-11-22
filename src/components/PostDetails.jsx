96dff13def258dd46a6d3ff92908f306f82eeb29
micheaol <micheaol80@gmail.com>
2021-11-22 14:29:58 +0100

    add delete event with redux

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 class PostDetails extends Component {
     render() {
@@ -16,7 +17,9 @@ class PostDetails extends Component {
                         <p>{post.content}</p>
                     </div>
                     <div className="card-action">
-                        <button className="btn black">Delete</button>
+                        <button onClick={()=> this.props.deletePost(post.id)} className="btn black">Delete
+                        <i className="material-icons right">delete</i>
+                        </button>
                     </div>
                 </div>
             </div>
@@ -24,4 +27,12 @@ class PostDetails extends Component {
     }
 }
 
-export default PostDetails
+const mapToDispatchToProps = (dispatch)=> {
+    return {
+        deletePost: (id)=>{
+            dispatch({ type: 'DELETE_POST', id})
+        }
+    }
+}
+
+export default connect(null, mapToDispatchToProps)(PostDetails)
--- src/store/reducers/postReducer.js
+++ src/store/reducers/postReducer.js
@@ -1,31 +1,39 @@
-
 const initState = {
-    posts: [
-        {
-            id:1,
-            title:"post one",
-            content: "This is post one"
-        },
-        {
-            id:2,
-            title:"post two",
-            content: "This is post two"
-        },
-        {
-            id:3,
-            title:"post three",
-            content: "This is post three"
-        },
-        {
-            id:4,
-            title:"post Four",
-            content: "This is post Four"
-        },
-    ]
-}
+  posts: [
+    {
+      id: 1,
+      title: 'post one',
+      content: 'This is post one',
+    },
+    {
+      id: 2,
+      title: 'post two',
+      content: 'This is post two',
+    },
+    {
+      id: 3,
+      title: 'post three',
+      content: 'This is post three',
+    },
+    {
+      id: 4,
+      title: 'post Four',
+      content: 'This is post Four',
+    },
+  ],
+};
 
-const postReducer = (state= initState, action)=> {
-    return state
-}
+const postReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'DELETE_POST':
+      const newPosts = state.posts.filter((post) => post.id !== action.id);
+      return {
+        posts: newPosts,
+      };
+      default:
+        return state;
+  }
+  
+};
 
 export default postReducer;
